55f445f5f7701a4e48d0c9861393bb170a9e59fb
Ellis Taylor <ellis.taylor@live.com>
2024-02-22 22:25:18 +0000

    add generic django view and modified settings so that it used it rather than the default

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,10 +28,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-quaim-djangoblog-4bceppyv5wi.ws-eu108.gitpod.io',
-                '.herokuapp.com']
+                '.herokuapp.com','.gitpod.io'
+]
 
 
 # Application definition
---
+++ env.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL", "postgres://esuavgdk:EOhc_aRbLHEogtqjWlk1EluIDtTV3Pn2@lucky.db.elephantsql.com/esuavgdk")
+os.environ.setdefault("SECRET_KEY", "zo~^=UtY!t6RR9@")
+
+    

d03741af0c5f1019fc5b840cf977c8f9c93d0001
Ellis Taylor <ellis.taylor@live.com>
2024-02-22 15:50:14 +0000

    added comment section and meta class to post and comment class to allow relevant informatoion to be used in the displaying of titles of posts/coments etc

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['8000-quaim-djangoblog-8mpidrxdq1u.ws-eu108.gitpod.io',
+ALLOWED_HOSTS = ['8000-quaim-djangoblog-4bceppyv5wi.ws-eu108.gitpod.io',
                 '.herokuapp.com']
 
 

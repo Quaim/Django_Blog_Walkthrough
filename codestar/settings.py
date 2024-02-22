da8a849b22abcad4f1f03a9a941a33a91db41768
Ellis Taylor <ellis.taylor@live.com>
2024-02-22 23:42:39 +0000

    added additonal functionality to admin page and added example blog posts from tutorial

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-quaim-djangoblog-4bceppyv5wi.ws-eu108.gitpod.io',
                 '.herokuapp.com','.gitpod.io'
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'django_summernote',
 ]
 
 MIDDLEWARE = [
